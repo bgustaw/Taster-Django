8fbe3731d6954473a1d3e43aade75c56477f5561
bgustaw <arnoldzrivii@gmail.com>
2023-11-05 21:25:16 +0100

    Deployed production version

--- populate_country_model.py
+++ populate_country_model.py
@@ -8,9 +8,9 @@ django.setup()
 from Taster.models import Country
 
 # read csv columns
-df = pd.read_csv("../static/country-codes_csv.csv",
+df = pd.read_csv("static/country-codes_csv.csv",
                  usecols=['ISO3166-1-Alpha-2', 'Continent', 'CLDR display name'])
-continent_names_df = pd.read_csv('../static/continent-codes_csv.csv')
+continent_names_df = pd.read_csv('static/continent-codes_csv.csv')
 
 continent_mapping = dict(zip(continent_names_df['Code'], continent_names_df['Name']))
 # replace short versions with full names
@@ -34,3 +34,4 @@ for index in range(len(df)):
     instance = Country(country=country, continent=continent, alpha2_code=alpha2_code)
     instance.save()
 
+print('Success')
